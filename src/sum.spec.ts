a80d72a6491690aee6ea2a6dad74414b3391ecd5
Bruno <brunonoriller@gmail.com>
2022-03-01 12:51:31 -0300

    feat: :sparkles: sum function

---
+++ src/sum.spec.ts
@@ -0,0 +1,54 @@
+import { Sum } from './sum';
+
+describe('sum', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(Sum('1', '2')).toStrictEqual('3');
+  });
+
+  it('adds 10 + 2 to equal 12', () => {
+    expect(Sum('10', '2')).toStrictEqual('12');
+  });
+
+  /**
+   * 123456789132456789 already overflows the number of digits in JavaScript
+   */
+  it('adds 123456789132456789 + 1 to equal 123456789132456790', () => {
+    expect(Sum('123456789132456789', '1')).toStrictEqual('123456789132456790');
+  });
+
+  it('adds 1 + 123456789132456789 to equal 123456789132456790', () => {
+    expect(Sum('1', '123456789132456789')).toStrictEqual('123456789132456790');
+  });
+
+  it('adds 1 + 1++ 1 to equal 3', () => {
+    expect(Sum('1', '1', '1')).toStrictEqual('3');
+  });
+
+  it('adds 0.1 + 1 to equal 1.1', () => {
+    expect(Sum('0.1', '1')).toStrictEqual('1.1');
+  });
+
+  it('adds 0.1 + 0.1 to equal 0.2', () => {
+    expect(Sum('0.1', '0.1')).toStrictEqual('0.2');
+  });
+
+  it('adds 0.01 + 0.001 to equal 0.011', () => {
+    expect(Sum('0.01', '0.001')).toStrictEqual('0.011');
+  });
+
+  it('adds 100.001 + 10.01 + 1.1 to equal 111.111', () => {
+    expect(Sum('100.001', '10.01', '1.1')).toStrictEqual('111.111');
+  });
+
+  it('adds 123456789.132456789 + 0.000000001 to equal 123456789132456790', () => {
+    expect(Sum('123456789.132456789', '0.000000001')).toStrictEqual('123456789.132456790');
+  });
+
+  it('adds 123456789.132456789 + 999999999.999999999 + 987654321.987654321 to equal 2111111111.120111109', () => {
+    expect(Sum('123456789.132456789', '999999999.999999999', '987654321.987654321')).toStrictEqual('2111111111.120111109');
+  });
+
+  it('adds 12345678913245678.9 + 9999999999999999.99 + 987654321987654.321 to equal 2111111111120111109', () => {
+    expect(Sum('12345678913245678.9', '9999999999999999.99', '987654321987654.321')).toStrictEqual('23333333235233333.211');
+  });
+});
---
+++ src/sum.ts
@@ -0,0 +1,56 @@
+export function Sum(...numbers: string[]): string {
+  const biggestFloat = Math.max(...numbers.map(n => getFloatPosition(n)));
+
+  const normalizedFloats = numbers.map(n => {
+    const currentFloat = getFloatPosition(n);
+    if (currentFloat === biggestFloat) {
+      return n;
+    }
+
+    const floatDiff = currentFloat === -1 ? 0 : currentFloat;
+    const repeatNeeded = biggestFloat - floatDiff;
+    return n + '0'.repeat(repeatNeeded);
+  });
+
+  const normalizedNumbers = normalizedFloats.map(n => {
+    return n.replace(/\D/g, '');
+  });
+
+  let numbersToSum = normalizedNumbers.reduce((acc, curr) => {
+    return Math.max(acc, curr.length);
+  }, 0);
+
+  const paddedNumbers = normalizedNumbers.map(n => n.padStart(numbersToSum, '0'));
+
+  let sum = '';
+  let carry = 0;
+
+  while (numbersToSum > 0) {
+    const digits = paddedNumbers.map(n => Number(n[numbersToSum - 1]));
+    let sumDigit = digits.reduce((acc, curr) => acc + curr, 0) + carry;
+    carry = 0;
+
+    while (sumDigit > 9 && numbersToSum > 1) {
+      sumDigit -= 10;
+      carry += 1;
+    }
+
+    sum = sumDigit.toString().concat(sum);
+    numbersToSum--;
+  }
+
+  if (biggestFloat > -1) {
+    const floatPosition = sum.length - biggestFloat;
+    return sum
+      .substring(0, floatPosition)
+      .concat('.')
+      .concat(sum.substring(floatPosition));
+  }
+
+  return sum;
+}
+
+function getFloatPosition(n: string): number {
+  const float = n.lastIndexOf('.');
+  return float === -1 ? -1 : n.length - float - 1;
+}
