3c685d51ad3d3873622c1befb9293396d759b901
Bruno <brunonoriller@gmail.com>
2022-03-04 20:04:01 -0300

    refactor: :recycle: better naming

--- src/operations/sum/sum.ts
+++ src/operations/sum/sum.ts
@@ -20,24 +20,24 @@ export function Sum(...numbers: string[]): string {
     return n.replace(/\D/g, '');
   });
 
-  let numbersToSum = normalizedNumbers.reduce((acc, curr) => {
+  let digitsToSum = normalizedNumbers.reduce((acc, curr) => {
     return Math.max(acc, curr.length);
   }, 0);
 
-  const paddedNumbers = normalizedNumbers.map(n => n.padStart(numbersToSum, '0'));
+  const paddedNumbers = normalizedNumbers.map(n => n.padStart(digitsToSum, '0'));
 
   let sum = '';
   let carry = BigInt(0);
 
-  while (numbersToSum > 0) {
-    const digits = paddedNumbers.map(n => Number(n[numbersToSum - 1]));
+  while (digitsToSum > 0) {
+    const digits = paddedNumbers.map(n => Number(n[digitsToSum - 1]));
     const sumDigit = (digits.reduce(
       (acc, curr) => BigInt(acc) + BigInt(curr), BigInt(0)
     ) + BigInt(carry)).toString().split('');
 
     sum = sumDigit.pop().concat(sum);
     carry = BigInt(sumDigit.join(''));
-    numbersToSum--;
+    digitsToSum--;
   }
 
   const carryLeft = carry === BigInt(0) ? '' : carry.toString();
